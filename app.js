a4d4765145c4dc53fc441e8a174dfb1750424bc8
jacksonarnold27 <jacksonarnold27@gmail.com>
2022-02-20 01:36:13 -0500

    init commit

---
+++ app.js
@@ -0,0 +1,32 @@
+// Part One: Solidify Terminology
+// In your own terms, define the following terms:
+
+// What is HTTP ?
+// HTTP is a protocol that computers, browsers, and networks use to communicate with each other
+
+// What is a URL ?
+// a URL is an address that points to a resource on the web (like a website)
+
+// What is DNS ?
+// DNS is a way for the internet to translate between IP addresses and domain names
+// What is a query string ?
+// The part of the URL that gets passed to the application/database
+
+// What are two HTTP verbs and how are they different ?
+// GET requests data from a server but doesn't change the server's data
+// POST requests data from a server and changes something, like data on the server
+// What is an HTTP request ?
+// a GET request that the browser makes to a server to get the HTML of a webpage
+
+// What is an HTTP response ?
+// the server sends back the HTML of a webpage back to the browser that requested it
+
+// What is an HTTP header ? Give a couple examples of request and response headers you have seen.
+// A HTTP header contains more information about the request/response being made: Host (hostname of a server), Location (a URL to redirect to), Referer (address of the page that brought you here), Date (date and time of request/response)
+
+// What are the processes that happen when you type “http://somesite.com/some/page.html” into a browser?
+// http://somesite.com/some/page.html gets turned into a numerical IP address
+// the browser connects to the IP address on a port using the HTTP protocol
+// the browser asks for /some/page.html
+
+
